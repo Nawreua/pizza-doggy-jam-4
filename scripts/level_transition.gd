5d34c750323a07c48969aa8d90a649ec2597e114
Nawreua <nawreua@gmail.com>
2026-02-27 19:55:32 +0100

    sounds: add door sound effect

--- scripts/level_transition.gd
+++ scripts/level_transition.gd
@@ -2,10 +2,12 @@ extends Area3D
 
 @export var scene: String = ""
 @export var stream: VideoStreamPlayer
+@export var audio: AudioStreamPlayer3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		body.process_mode = Node.PROCESS_MODE_DISABLED
+		audio.play()
 		stream.play()
 
 func _on_video_stream_player_finished() -> void:
