d16127c543ebab796c4e39a690118c282930f8ab
Nawreua <nawreua@gmail.com>
2026-03-05 18:25:00 +0100

    script: reworked transition script

--- scripts/level_transition.gd
+++ scripts/level_transition.gd
@@ -7,7 +7,8 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		body.process_mode = Node.PROCESS_MODE_DISABLED
-		audio.play()
+		if audio:
+			audio.play()
 		stream.play()
 
 func _on_video_stream_player_finished() -> void:
